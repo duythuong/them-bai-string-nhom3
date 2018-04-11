59759932afa6795a942c4015a90cfbacaa294803
= <=>
2018-04-11 16:38:50 +0700

    bai6.java

---
+++ bai6.java
@@ -0,0 +1,12 @@
+package array;
+
+public class bai6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s = "nguyen duy thuong";
+ int t=s.length();
+ System.out.println(t);
+	}
+
+}
